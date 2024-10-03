6ca6c1a567f78e231d31e99e1f827f8747b40627
Oleg Makarov <makarovolegbox@yandex.ru>
2024-10-03 20:37:40 +0300

    update

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -1,6 +1,14 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
